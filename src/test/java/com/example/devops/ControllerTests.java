92eb797ab28934c341aeebd5fd07da9528085083
Emily Bordelon <emilynbordelon@gmail.com>
2023-08-25 15:52:48 -0500

    First commit

---
+++ src/test/java/com/example/devops/ControllerTests.java
@@ -0,0 +1,22 @@
+package com.example.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(Controller.class)
+public class ControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello DevOps!!"));
+    }
+}
